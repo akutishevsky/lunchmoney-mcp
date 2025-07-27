17bb83dc735a92eee5d6f2d03a6b20b0f492d643
Anton Kutishevsky <akutishevsky@gmail.com>
2025-07-27 07:59:12 +0300

    feat: added Category and CategoryChild interfaces to types

--- src/types.ts
+++ src/types.ts
@@ -7,3 +7,28 @@ export interface User {
     primary_currency: string;
     api_key_label: string | null;
 }
+
+export interface CategoryChild {
+    id: number;
+    name: string;
+    description: string | null;
+    created_at: string;
+}
+
+export interface Category {
+    id: number;
+    name: string;
+    description?: string | null;
+    is_income: boolean;
+    exclude_from_budget: boolean;
+    exclude_from_totals: boolean;
+    archived: boolean | null;
+    archived_on: string | null;
+    updated_at: string | null;
+    created_at: string | null;
+    is_group: boolean;
+    group_id: number | null;
+    order: number | null;
+    children?: CategoryChild[] | null;
+    group_category_name?: string | null;
+}
